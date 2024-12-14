f18601cc62e79702e75e889c8abcf00394c6f46f
somnath-project <somnath.ekhande99@gmail.com>
2024-12-14 17:30:43 +0530

    querySelector

--- force-app/main/default/lwc/dynamicCss/dynamicCss.js
+++ force-app/main/default/lwc/dynamicCss/dynamicCss.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class DynamicCss extends LightningElement {}
+export default class DynamicCss extends LightningElement {
+
+    pcolor="chocolate-background";
+   
+    addCssHandler(event){
+        let element=this.template.querySelector("p");
+        element.classList.add("green-border");
+    }
+    removeCssHandler(event){
+        let element=this.template.querySelector("p");
+        element.classList.remove("green-border");
+    }
+    
+    toggleCssHandler(event){
+        let element=this.template.querySelector("p");
+        element.classList.toggle("green-border");
+    }
+
+}
